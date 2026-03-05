c7b3e230de99badcdca5c3d0b6db2be97c431432
kirill <kirill@kirill.kirill>
2026-03-05 14:28:41 -0500

    final

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 template< class T >
 T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c) {
   size_t k = 0;
-  for (size_t i = 0, j = 0; i < sa && j < sb; ) {
+  for (size_t i = 0, j = 0; i < sa && j < sb; k++) {
     if (a[i] < b[j]) {
-      c[k++] = a[i++];
+      c[k] = a[i++];
     } else {
-      c[k++] = b[j++];
+      c[k] = b[j++];
     }
   }
+  while (i < sa)
+    c[k++] = a[i++];
+  while (j < sb)
+    c[k++] = b[j++];
+  return c;
 }
